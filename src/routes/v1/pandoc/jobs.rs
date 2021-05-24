719ce4e0aac2ae76126372cd53c1d395ddedb65c
Christian Fries <christian.fries123@gmail.com>
2021-05-24 10:04:38 +0200

    use uuid to identify jobs

--- src/routes/v1/pandoc/jobs.rs
+++ src/routes/v1/pandoc/jobs.rs
@@ -1,28 +1,37 @@
 use serde::{Serialize, Deserialize};
 use chrono::{DateTime, Utc};
 use rocket_contrib::json::Json;
+use rocket_contrib::uuid::Uuid;
 use rocket_okapi::swagger_ui::*;
 use schemars::JsonSchema;
 
 
 #[derive(Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
-pub struct Jobs {
-   pub name: String,
+pub struct Job {
+   pub id: Uuid,
 }
 
 #[openapi]
 #[get("/jobs")]
-pub fn jobs() -> Json<Vec<Jobs>> {
+pub fn jobs() -> Json<Vec<Job>> {
    let mut jobs = Vec::new();
 
-   jobs.push(Jobs{
-      name: "Job1".to_string(),
+   jobs.push(Job {
+      id: Uuid::new_v4(),
    });
 
-   jobs.push(Jobs{
-      name: "Job2".to_string(),
+   jobs.push(Job {
+      id: Uuid::new_v4(),
    });
 
    Json(jobs)
+}
+
+#[openapi]
+#[post("/jobs")]
+pub fn create() -> Json<Job> {
+   Json( Job {
+      id: Uuid::new_v4(),
+   })
 }
