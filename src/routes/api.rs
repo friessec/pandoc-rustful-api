8cbd855fd5f77476faf891e2b9fd8d4ab03da07e
FriesSec <christian.fries123@gmail.com>
2021-12-30 16:12:37 +0100

    Dummy API definition

--- src/routes/api.rs
+++ src/routes/api.rs
@@ -10,7 +10,20 @@ pub fn configuration(cfg: &mut web::ServiceConfig) {
                         web::resource("")
                             .route(web::get().to(jobs::job_list))
                             .route(web::post().to(jobs::job_create)),
-                    ),
+                    )
+                    .service(
+                        web::resource("/{id}")
+                            .route(web::get().to(jobs::job_get))
+                            .route(web::delete().to(jobs::job_delete))
+                    )
+                    .service(
+                        web::resource("/{id}/upload")
+                            .route(web::post().to(jobs::job_upload))
+                    )
+                    .service(
+                        web::resource("/{id}/process")
+                            .route(web::get().to(jobs::job_process))
+                    )
             )
     );
 }
--- src/routes/v1/jobs.rs
+++ src/routes/v1/jobs.rs
@@ -1,15 +1,15 @@
-use actix_web::{web, Result};
 use serde::{Serialize, Deserialize};
 use paperclip::actix::{
     Apiv2Schema,
     api_v2_operation,
     web::Json,
+    web::Path,
 };
 
 #[derive(Serialize, Deserialize, Apiv2Schema)]
 pub struct Job {
     name: String,
-    id: Option<i64>,
+    id: Option<u32>,
 }
 
 #[api_v2_operation]
@@ -18,7 +18,7 @@ pub async fn job_list() -> Result<Json<Job>, ()> {
         name: "Test".to_string(),
         id: None,
     };
-    Ok(web::Json(job))
+    Ok(Json(job))
 }
 
 #[api_v2_operation]
@@ -27,5 +27,41 @@ pub async fn job_create() -> Result<Json<Job>, ()> {
         name: "Test".to_string(),
         id: None,
     };
-    Ok(web::Json(job))
+    Ok(Json(job))
+}
+
+#[api_v2_operation]
+pub async fn job_get(path: Path<(u32,)>) -> Result<Json<Job>, ()> {
+    let job = Job {
+        name: "Test".to_string(),
+        id: Option::from(path.into_inner().0),
+    };
+    Ok(Json(job))
+}
+
+#[api_v2_operation]
+pub async fn job_delete(path: Path<(u32,)>) -> Result<Json<Job>, ()> {
+    let job = Job {
+        name: "Test".to_string(),
+        id: Option::from(path.into_inner().0),
+    };
+    Ok(Json(job))
+}
+
+#[api_v2_operation]
+pub async fn job_upload(path: Path<(u32,)>) -> Result<Json<Job>, ()> {
+    let job = Job {
+        name: "Test".to_string(),
+        id: Option::from(path.into_inner().0),
+    };
+    Ok(Json(job))
+}
+
+#[api_v2_operation]
+pub async fn job_process(path: Path<(u32,)>) -> Result<Json<Job>, ()> {
+    let job = Job {
+        name: "Test".to_string(),
+        id: Option::from(path.into_inner().0),
+    };
+    Ok(Json(job))
 }
