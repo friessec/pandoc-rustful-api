ea3716688bef1e7e00079462620709e4c680d424
FriesSec <christian.fries123@gmail.com>
2021-12-31 12:54:26 +0100

    Add constants.rs for generic constant values Create a work directory

---
+++ src/configs/constants.rs
@@ -0,0 +1,4 @@
+// Default Configurations
+pub const DEFAULT_INTERFACE: &str = "127.0.0.1";
+pub const DEFAULT_PORT: &str = "8000";
+pub const DEFAULT_WORKDIR: &str = "/tmp/pandoc-workdir";
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 extern crate actix_web;
 extern crate log;
 
+mod configs;
 mod routes;
 mod models;
 
 use actix_web::{App, HttpServer};
 use paperclip::actix::OpenApiExt;
+use configs::constants;
+use crate::constants::*;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG",
                       "actix_web=debug,actix_server=debug,pandoc_rustful_api=debug");
+    std::env::set_var("WORKDIR",
+                      DEFAULT_WORKDIR);
     env_logger::init();
+
+    let bind_addr = format!("{}:{}", DEFAULT_INTERFACE, DEFAULT_PORT);
 
     let server = HttpServer::new(move || {
         App::new()
@@ -23,9 +30,28 @@ async fn main() -> std::io::Result<()> {
             // Mount swagger-ui after build
             .configure(routes::swagger::configuration)
     })
-        .bind("127.0.0.1:8080")?;
+        .bind(bind_addr.clone())?;
 
+    create_work_directory()?;
     log::info!("Starting http server");
-
+    log::info!("Swagger-UI: http://{}/swagger-ui/", bind_addr);
     server.run().await
+}
+
+fn create_work_directory() -> std::io::Result<()> {
+    let workdir = std::env::var("WORKDIR").unwrap_or(DEFAULT_WORKDIR.to_string());
+    let dir = std::path::Path::new(workdir.as_str());
+    if dir.exists() {
+        return if dir.is_dir() {
+            log::debug!("Using work directory: {}", dir.display());
+            Ok(())
+        } else {
+            Err(std::io::Error::new(std::io::ErrorKind::AlreadyExists,
+                                    "Provided path exists but is not a directory!"))
+        };
+    }
+    std::fs::create_dir(dir)?;
+
+    log::debug!("Created work directory: {}", dir.display());
+    Ok(())
 }
