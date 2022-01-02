a44264cb6c2c3f23f326cc95a5cd89ff17d5852a
FriesSec <christian.fries123@gmail.com>
2022-01-02 09:23:52 +0100

    Multipart file upload

--- src/routes/v1/jobs.rs
+++ src/routes/v1/jobs.rs
@@ -1,8 +1,11 @@
-use paperclip::actix::{
-    api_v2_operation,
-    web::Json,
-    web::Path,
-};
+use std::io::Write;
+use actix_multipart::Multipart;
+use actix_web::Error;
+use futures_util::TryStreamExt as _;
+use paperclip::actix::{api_v2_operation, web, web::Json, web::Path};
+use paperclip::actix::web::HttpResponse;
+use uuid::Uuid;
+use crate::DEFAULT_WORKDIR;
 
 use crate::models::job::Job;
 use crate::services::job_service;
@@ -19,7 +22,7 @@ pub async fn job_create() -> Result<Json<Job>, ()> {
 }
 
 #[api_v2_operation]
-pub async fn job_get(path: Path<(uuid::Uuid,)>) -> Result<Json<Job>, ()> {
+pub async fn job_get(path: Path<(uuid::Uuid, )>) -> Result<Json<Job>, ()> {
     let job = Job {
         id: Option::from(path.into_inner().0),
     };
@@ -27,7 +30,7 @@ pub async fn job_get(path: Path<(uuid::Uuid,)>) -> Result<Json<Job>, ()> {
 }
 
 #[api_v2_operation]
-pub async fn job_delete(path: Path<(uuid::Uuid,)>) -> Result<Json<Job>, ()> {
+pub async fn job_delete(path: Path<(uuid::Uuid, )>) -> Result<Json<Job>, ()> {
     let job = Job {
         id: Option::from(path.into_inner().0),
     };
@@ -35,17 +38,31 @@ pub async fn job_delete(path: Path<(uuid::Uuid,)>) -> Result<Json<Job>, ()> {
 }
 
 #[api_v2_operation]
-pub async fn job_upload(path: Path<(uuid::Uuid,)>) -> Result<Json<Job>, ()> {
-    let job = Job {
-        id: Option::from(path.into_inner().0),
-    };
-    Ok(Json(job))
+pub async fn job_upload(path: Path<(uuid::Uuid, )>, mut payload: Multipart) -> Result<HttpResponse, Error> {
+    let id = path.into_inner().0;
+    while let Some(mut field) = payload.try_next().await? {
+        let content_disposition = field
+            .content_disposition()
+            .ok_or_else(|| HttpResponse::BadRequest().finish())?;
+
+        let filename = content_disposition.get_filename().map_or_else(
+            || Uuid::new_v4().to_string(),
+            |file| sanitize_filename::sanitize(file),
+        );
+
+        let filepath = format!("{}/{}/{}", DEFAULT_WORKDIR, id, filename);
+
+        let mut file = web::block(|| std::fs::File::create(filepath)).await?;
+        while let Some(chunk) = field.try_next().await? {
+            file = web::block(move || file.write_all(&chunk).map(|_| file)).await?;
+        }
+    }
+
+    Ok(HttpResponse::Ok().into())
 }
 
 #[api_v2_operation]
-pub async fn job_process(path: Path<(uuid::Uuid,)>) -> Result<Json<Job>, ()> {
-    let job = Job {
-        id: Option::from(path.into_inner().0),
-    };
-    Ok(Json(job))
+pub async fn job_process(path: Path<(uuid::Uuid, )>) -> Result<HttpResponse, Error> {
+
+    Ok(HttpResponse::Ok().into())
 }
